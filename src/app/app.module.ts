006ff819b244856dd180bdc75309a1423523efc7
Samuel1418 <sgomezpintos@danielcastelao.org>
2020-02-20 12:11:48 +0100

    lista con carrito y mostrador hecha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './producto/producto.component';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoComponent,
+    ListaProductosComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })

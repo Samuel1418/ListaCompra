69f49f7a54418a5eaababa687d629ff9cb7aede9
Samuel1418 <sgomezpintos@danielcastelao.org>
2020-03-11 10:46:42 +0100

    Añadidos los botones, sus funciones y funciona la autentificacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import {environment} from '../environments/environment';
+import {ServicioDeAutentService} from './servicio-de-autent.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ShopList';
+
+
+  constructor(public authComponent: ServicioDeAutentService) {
+
+  }
 }
--- src/app/servicio-de-autent.service.ts
+++ src/app/servicio-de-autent.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
+import {auth} from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioDeAutentService {
 
+  authUser = null;
+
   constructor(public miauth: AngularFireAuth) { }
+  user = this.miauth.authState;
+  login() {
+    console.log('login!');
+  }
+  glogin() {
+    console.log('google login!');
+    this.miauth.auth.signInWithPopup( new auth.GoogleAuthProvider() )
+      .then( user => {
+        console.log('user logueado: ', user);
+        this.authUser = user.user;
+      })
+      .catch( error => {
+        console.log('error en google login: ', error);
+      });
+  }
+  logout() {
+    console.log('logout!');
+    this.miauth.auth.signOut();
+  }
 }
